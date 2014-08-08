cc1d5801d19f4341911709d7a2b1d26fef386d33
Daniel Thengvall <fender5289@gmail.com>
2014-08-08 15:26:02 -0700

    set up table view structure

--- ios/DTHENG/DTHENG/BaseTableViewController.h
+++ ios/DTHENG/DTHENG/BaseTableViewController.h
@@ -1,12 +1,6 @@
-//
-//  BaseTableViewController.h
-//  DTHENG
-//
-//  Created by Daniel Thengvall on 8/8/14.
-//  Copyright (c) 2014 Daniel Thengvall. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+
+// author : Daniel Thengvall
 
 @interface BaseTableViewController : UITableViewController
 
