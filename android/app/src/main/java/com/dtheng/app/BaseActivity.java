5c043ec1dc5b9516bb2d3ec68ae2e89e5fe32bc2
Daniel Thengvall <fender5289@gmail.com>
2014-08-08 17:01:05 -0700

    setup home activity to extend base activity

--- android/app/src/main/java/com/dtheng/app/BaseActivity.java
+++ android/app/src/main/java/com/dtheng/app/BaseActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/** author : Daniel Thengvall **/
 
 public class BaseActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        setContentView(R.layout.activity_home);
     }
 
 
