2c36eb5eb44d8a9061a5f8e58629996fddd950f6
Daniel Thengvall <fender5289@gmail.com>
2014-08-09 18:08:46 -0700

    removed unused code

--- ios/DTHENG/DTHENG/AppDelegate.m
+++ ios/DTHENG/DTHENG/AppDelegate.m
@@ -4,18 +4,4 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    return YES;
-}
-							
-- (void)applicationWillResignActive:(UIApplication *)application { }
-
-- (void)applicationDidEnterBackground:(UIApplication *)application { }
-
-- (void)applicationWillEnterForeground:(UIApplication *)application { }
-
-- (void)applicationDidBecomeActive:(UIApplication *)application { }
-
-- (void)applicationWillTerminate:(UIApplication *)application { }
-
 @end
