883804c3212c6c633537a0a99908229d8249deb7
Daniel Thengvall <fender5289@gmail.com>
2014-08-11 22:52:22 -0700

    setup new tableview for resume

--- ios/DTHENG/DTHENG/ResumeTableViewController.m
+++ ios/DTHENG/DTHENG/ResumeTableViewController.m
@@ -8,4 +8,37 @@
 
 @implementation ResumeTableViewController
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch (indexPath.row) {
+        case 0:
+            return 162;
+        case 1:
+            return 400;
+        default:
+            return 100;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch (indexPath.row) {
+            
+        case 0:
+            return [tableView dequeueReusableCellWithIdentifier:@"yourCompanyCellPrototype" forIndexPath:indexPath];
+        case 1:
+            return [tableView dequeueReusableCellWithIdentifier:@"rixtyCellPrototype" forIndexPath:indexPath];
+        case 2:
+            return [tableView dequeueReusableCellWithIdentifier:@"artSchoolCellPrototype" forIndexPath:indexPath];
+        case 3:
+            return [tableView dequeueReusableCellWithIdentifier:@"startedCodingCellPrototype" forIndexPath:indexPath];
+        default:
+            return [tableView dequeueReusableCellWithIdentifier:@"artSchoolCellPrototype" forIndexPath:indexPath];
+
+
+    }
+}
+
 @end
