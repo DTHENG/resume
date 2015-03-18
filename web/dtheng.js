06419859c6a7dd70e44f4ba0e33d6f5856d4f7dd
DTHENG <fender5289@gmail.com>
2015-03-17 20:32:54 -0700

    updated css

--- web/dtheng.js
+++ web/dtheng.js
@@ -20,8 +20,15 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'DTHENG' }
+  res.render('index', {
+        title: 'DTHENG',
+        github_link: 'https://github.com/DTHENG',
+        angellist_link: 'https://angel.co/daniel-thengvall',
+        twitter_link: 'https://twitter.com/DTHENG',
+        instagram_link: 'http://instagram.com/dtheng',
+        snapcard_link: 'https://twitter.com/JoinSnapCard',
+        rixty_link: 'https://twitter.com/rixty'   
+    }
   )
 })
 
