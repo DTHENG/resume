9313dc771342cd923f5166c13e8f1c56d1872f4d
Daniel Thengvall <dan@joinsnapcard.com>
2023-07-08 15:47:12 -0700

    Github actions (#53)

--- pdf-gen-standalone.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:current-alpine3.13
-
-RUN mkdir -p /pdf-gen
-WORKDIR /pdf-gen
-COPY pdf-gen/ .
-RUN npm install
--- pdf-gen/src/pdf-gen.ts
+++ pdf-gen/src/pdf-gen.ts
@@ -1,9 +1,7 @@
 import { existsSync, mkdirSync } from 'fs';
-import { uploadFile } from './gcs-client';
 import { buildResume } from './pdf-builder';
 
-export const buildAndUpload = async (): Promise<void> => {
+export const build = async (): Promise<void> => {
   if (!existsSync('tmp')) mkdirSync('tmp');
   await buildResume('tmp/DanielThengvallResume.pdf');
-  await uploadFile('tmp/DanielThengvallResume.pdf', 'com-dtheng');
 };
